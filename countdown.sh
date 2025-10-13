613e2dd7c206f61cf03a28670716f0169846fd7d
chasejones01 <chase.jones@tcu.edu>
2025-10-12 20:12:37 -0500

    Added user input for N in countdown.sh

---
+++ countdown.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# countdown.sh performs a countdown
+
+read -p "Enter a positive number: " N
+
+if [ ! -n "$N" ] || [ "$N" -le 0 ]
+then
+	echo "Error: Input must be a positive number."
+	exit 1
+fi
