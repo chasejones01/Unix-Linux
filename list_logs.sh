6a5de2044fad25ff86873dfc350f48a861db9a4d
chasejones01 <chase.jones@tcu.edu>
2025-10-14 01:31:13 -0500

    Added counter and summary output to list_logs.sh

--- list_logs.sh
+++ list_logs.sh
@@ -2,9 +2,13 @@
 
 # list_logs.sh lists all log files in logs/ directory
 
+COUNT=0
 echo "Listing log files in logs/:"
 
 for FILE in logs/*.log
 do
 	echo "$FILE"
+	COUNT=$((COUNT + 1))
 done
+
+echo "Total number of log files: $COUNT"
