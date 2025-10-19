b5889ff9b49d45b251eea92f25d7537d3c7cac9b
chasejones01 <chase.jones@tcu.edu>
2025-10-19 18:53:25 -0500

    Finalized interactive system_report.sh menu

--- system_report.sh
+++ system_report.sh
@@ -12,26 +12,30 @@ function show_disk() {
 	df -h
 }
 
-echo "1) Show system uptime"
-echo "2) Show disk usage"
-echo "3) Exit"
+CHOICE=0
+while [ "$CHOICE" -ne 3 ];
+do
+	echo "1) Show system uptime"
+	echo "2) Show disk usage"
+	echo "3) Exit"
 
-read -p "Enter your choice: " CHOICE
+	read -p "Enter your choice: " CHOICE
 
-case $CHOICE in
-	1)
-	      	show_uptime
-		;;
+	case $CHOICE in
+		1)
+		      	show_uptime
+			;;
 	
-	2)
-		show_disk
-		;;
+		2)
+			show_disk
+			;;
 	
-	3)
-		exit
-		;;
+		3)
+			echo "System report closed."
+			;;
 	
-	*)
-		echo "Invalid choice"
-		;;
-esac
+		*)
+			echo "Invalid choice"
+			;;
+	esac
+done
