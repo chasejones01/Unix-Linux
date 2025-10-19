c8018a5c26f11f3934c1455404677c280af34670
chasejones01 <chase.jones@tcu.edu>
2025-10-19 18:47:55 -0500

    Implemented menu options in system_report.sh

--- system_report.sh
+++ system_report.sh
@@ -12,5 +12,26 @@ function show_disk() {
 	df -h
 }
 
-show_uptime
-show_disk
+echo "1) Show system uptime"
+echo "2) Show disk usage"
+echo "3) Exit"
+
+read -p "Enter your choice: " CHOICE
+
+case $CHOICE in
+	1)
+	      	show_uptime
+		;;
+	
+	2)
+		show_disk
+		;;
+	
+	3)
+		exit
+		;;
+	
+	*)
+		echo "Invalid choice"
+		;;
+esac
